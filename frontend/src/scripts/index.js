dc6beff0c03c7de260ea10ba7b0f795114ff9fe2
Raglicia Sandi <ragliciasousa@gmail.com>
2024-07-20 15:28:02 -0300

    Adição de navegação entre as telas

--- frontend/src/scripts/index.js
+++ frontend/src/scripts/index.js
@@ -1,8 +1,51 @@
 function preventSubmit(event) {
-    event.preventDefault()
-    window.location.href = "./pages/recepcionista/home.html"
+  event.preventDefault();
+  window.location.href = "./pages/recepcionista/home.html";
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-    document.getElementById('login-form').addEventListener('submit', preventSubmit);
+document.addEventListener("DOMContentLoaded", function () {
+  document
+    .getElementById("login-form")
+    .addEventListener("submit", preventSubmit);
 });
+
+// navegação das telas gerente
+function irTelaPacientes() {
+  window.location.href = "../recepcionista/paciente.html";
+}
+
+function irTelaAgendamento() {
+  window.location.href = "../recepcionista/agenda.html";
+}
+
+function irTelaDentista() {
+  window.location.href = "../gerente/dentista.html";
+}
+
+function irTelaFuncionario() {
+  window.location.href = "../gerente/funcionario.html";
+}
+
+function irTelaCadastrarDentista() {
+  window.location.href = "../recepcionista/cadastrar_dentista.html";
+}
+
+// navegação das telas dentista
+function irTelaPacientesDentista() {
+  window.location.href = "../dentista/Pacientes.html";
+}
+function irTelaProntuarioDentista() {
+  window.location.href = "../dentista/prontuario_dentista.html";
+}
+
+function irTelaAgendaDentista() {
+  window.location.href = "../dentista/agenda.html";
+}
+
+function irTelaCadastrarConsulta() {
+  window.location.href = "../gerente/calendario.html";
+}
+
+function irTelaCriarProntuario() {
+  window.location.href = "../dentista/prontuario.html";
+}
